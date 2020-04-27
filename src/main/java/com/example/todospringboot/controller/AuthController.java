629b59198863e836627db7938ec7b4e5a2b532a9
Sagar Mamdapure <sagarmamdapure@gmail.com>
2020-04-27 09:06:11 +0530

    1. Implemented forget password functionality 2. Added missing maven dependency in pom.xml

--- src/main/java/com/example/todospringboot/controller/AuthController.java
+++ src/main/java/com/example/todospringboot/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.example.todospringboot.controller;
 import com.example.todospringboot.entity.Role;
 import com.example.todospringboot.entity.User;
 import com.example.todospringboot.models.ERole;
+import com.example.todospringboot.models.ForgetUserPayload;
 import com.example.todospringboot.payload.request.LoginRequest;
 import com.example.todospringboot.payload.request.SignupRequest;
 import com.example.todospringboot.payload.response.JwtResponse;
@@ -11,6 +12,7 @@ import com.example.todospringboot.security.UserDetailsImpl;
 import com.example.todospringboot.security.jwt.JwtUtils;
 import com.example.todospringboot.service.RoleRepositoryService;
 import com.example.todospringboot.service.UserRepositoryService;
+import com.example.todospringboot.utils.AwsRdsCredentialUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,6 +25,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -38,6 +41,8 @@ public class AuthController {
 
   final RoleRepositoryService roleRepositoryService;
 
+  final AwsRdsCredentialUtil awsRdsCredentialUtil;
+
   final PasswordEncoder encoder;
 
   final JwtUtils jwtUtils;
@@ -47,22 +52,24 @@ public class AuthController {
           AuthenticationManager authenticationManager,
           UserRepositoryService userRepositoryService,
           RoleRepositoryService roleRepositoryService,
+          AwsRdsCredentialUtil awsRdsCredentialUtil,
           PasswordEncoder encoder,
           JwtUtils jwtUtils) {
-      this.authenticationManager = authenticationManager;
-      this.userRepositoryService = userRepositoryService;
-      this.roleRepositoryService = roleRepositoryService;
-      this.encoder = encoder;
-      this.jwtUtils = jwtUtils;
+    this.authenticationManager = authenticationManager;
+    this.userRepositoryService = userRepositoryService;
+    this.roleRepositoryService = roleRepositoryService;
+    this.awsRdsCredentialUtil = awsRdsCredentialUtil;
+    this.encoder = encoder;
+    this.jwtUtils = jwtUtils;
   }
 
   @PostMapping("/signin")
   public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
-      Authentication authentication =
-              authenticationManager.authenticate(
-                      new UsernamePasswordAuthenticationToken(
-                              loginRequest.getUsername(), loginRequest.getPassword()));
+    Authentication authentication =
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(), loginRequest.getPassword()));
 
     try {
       SecurityContext ctx = SecurityContextHolder.createEmptyContext();
@@ -70,21 +77,21 @@ public class AuthController {
       ctx.setAuthentication(authentication);
       String jwt = jwtUtils.generateJwtToken(authentication);
 
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles =
-                userDetails.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .collect(Collectors.toList());
+      UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+      List<String> roles =
+              userDetails.getAuthorities().stream()
+                      .map(GrantedAuthority::getAuthority)
+                      .collect(Collectors.toList());
 
-        return ResponseEntity.ok(
-                new JwtResponse(
-                        jwt,
-                        userDetails.getId(),
-                        userDetails.getUsername(),
-                        userDetails.getEmail(),
-                        roles,
-                        userDetails.getFirstName(),
-                        userDetails.getLastName()));
+      return ResponseEntity.ok(
+              new JwtResponse(
+                      jwt,
+                      userDetails.getId(),
+                      userDetails.getUsername(),
+                      userDetails.getEmail(),
+                      roles,
+                      userDetails.getFirstName(),
+                      userDetails.getLastName()));
     } finally {
       SecurityContextHolder.clearContext();
     }
@@ -93,60 +100,61 @@ public class AuthController {
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
     if (userRepositoryService.existsByUsername(signUpRequest.getUsername())) {
-        return ResponseEntity.badRequest()
-                .body(new MessageResponse("Error: Username is already taken!"));
+      return ResponseEntity.badRequest()
+              .body(new MessageResponse("Error: Username is already taken!"));
     }
 
     if (userRepositoryService.existsByEmail(signUpRequest.getEmail())) {
-        return ResponseEntity.badRequest()
-                .body(new MessageResponse("Error: Email is already in use!"));
+      return ResponseEntity.badRequest()
+              .body(new MessageResponse("Error: Email is already in use!"));
     }
 
     // Create new user's account
-      User user =
-              new User(
-                      signUpRequest.getUsername(),
-                      signUpRequest.getEmail(),
-                      encoder.encode(signUpRequest.getPassword()),
-                      signUpRequest.getFirstName(),
-                      signUpRequest.getLastName());
+    User user =
+            new User(
+                    signUpRequest.getUsername(),
+                    signUpRequest.getEmail(),
+                    encoder.encode(signUpRequest.getPassword()),
+                    signUpRequest.getFirstName(),
+                    signUpRequest.getLastName(),
+                    signUpRequest.getContact());
 
     Set<String> strRoles = signUpRequest.getRole();
     Set<Role> roles = new HashSet<>();
 
     if (strRoles == null) {
-        Role userRole =
-                roleRepositoryService
-                        .findByName(ERole.ROLE_USER)
-                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+      Role userRole =
+              roleRepositoryService
+                      .findByName(ERole.ROLE_USER)
+                      .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
       roles.add(userRole);
     } else {
       strRoles.forEach(
               role -> {
-                  switch (role) {
-                      case "admin":
-                          Role adminRole =
-                                  roleRepositoryService
-                                          .findByName(ERole.ROLE_ADMIN)
-                                          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                          roles.add(adminRole);
+                switch (role) {
+                  case "admin":
+                    Role adminRole =
+                            roleRepositoryService
+                                    .findByName(ERole.ROLE_ADMIN)
+                                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(adminRole);
 
-                          break;
-                      case "mod":
-                          Role modRole =
-                                  roleRepositoryService
-                                          .findByName(ERole.ROLE_MODERATOR)
-                                          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                          roles.add(modRole);
+                    break;
+                  case "mod":
+                    Role modRole =
+                            roleRepositoryService
+                                    .findByName(ERole.ROLE_MODERATOR)
+                                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(modRole);
 
-                          break;
-                      default:
-                          Role userRole =
-                                  roleRepositoryService
-                                          .findByName(ERole.ROLE_USER)
-                                          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-                          roles.add(userRole);
-                  }
+                    break;
+                  default:
+                    Role userRole =
+                            roleRepositoryService
+                                    .findByName(ERole.ROLE_USER)
+                                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(userRole);
+                }
               });
     }
 
@@ -154,5 +162,17 @@ public class AuthController {
     userRepositoryService.save(user);
 
     return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+  }
+
+  @PostMapping("/forget")
+  public String forgetPassword(@RequestBody ForgetUserPayload forgetUser)
+          throws URISyntaxException {
+
+    return userRepositoryService.forgetPassword(forgetUser);
+  }
+
+  @PostMapping("/changepassword")
+  public Boolean changePassword(@RequestBody ForgetUserPayload forgetUser) {
+    return userRepositoryService.changePassword(forgetUser);
   }
 }
